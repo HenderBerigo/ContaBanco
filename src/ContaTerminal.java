619f4e568e22d35f57ef15c5b7c29d7459fff7dc
Hender Berigo <henderson.berigo@gmail.com>
2025-02-10 07:08:47 -0300

    ajuste no scanner Parse, conta e saldo

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -11,16 +11,21 @@ public class ContaTerminal {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("-- Bem vindo(a) ao Banco XX");
+
         System.out.print("Por favor, digite o Número da Agência: ");
         agenciaConta = scan.nextLine();
+
         System.out.print("Por favor, digite o número da Conta: ");
-        numeroConta = scan.nextInt();
+        numeroConta = Integer.parseInt(scan.nextLine());
+
         System.out.print("Por favor, digite o seu nome completo: ");
         nomeCliente = scan.nextLine();
-        System.out.print("Por favor, digite o seu Saldo: ");
-        saldoConta = scan.nextDouble();
-        System.out.println("---------");
-        System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agenciaConta+", conta "+numeroConta+" e seu saldo de "+saldoConta+" já está disponível para saque.");
 
+        System.out.print("Por favor, digite o seu Saldo: ");
+        saldoConta = Double.parseDouble(scan.nextLine());
+
+        System.out.println("---------");
+        System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco,\nsua agência é "+agenciaConta+", conta "+numeroConta+" e seu saldo de "+saldoConta+" já está disponível para saque.");
+        scan.close();
     }
 }
